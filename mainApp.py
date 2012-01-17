8d0e888ba1bba1ae2ca2fdba9b789d569db77267
Jon Plaut <jp@tangerinesymphony.net>
2012-01-17 13:45:32 -0800

    up and running with edev and prod environments

--- mainApp.py
+++ mainApp.py
@@ -9,18 +9,21 @@ from flask import Flask, request, redirect
 from mako.template import Template
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_object('conf.Config')
+
 if os.environ.get('FACEBOOK_APP_ID'):
-    FBAPI_APP_ID = os.environ.get('FACEBOOK_APP_ID')
-    FBAPI_APP_SECRET = os.environ.get('FACEBOOK_SECRET')
+    APP_ID = os.environ.get('FACEBOOK_APP_ID')
+    APP_SECRET = os.environ.get('FACEBOOK_SECRET')
 else:
-    FBAPI_APP_ID = app.config['FBAPI_APP_ID']
-    FBAPI_APP_SECRET = FBAPI_APP_SECRET
+    APP_ID = app.config.get('FBAPI_APP_ID')
+    APP_SECRET = app.config.get('FBAPI_APP_SECRET')
 
 
 def oauth_login_url(preserve_path=True, next_url=None):
     fb_login_uri = ("https://www.facebook.com/dialog/oauth"
                     "?client_id=%s&redirect_uri=%s" %
-                    (FBAPI_APP_ID, get_home()))
+                    (APP_ID, get_home()))
 
     if app.config['FBAPI_SCOPE']:
         fb_login_uri += "&scope=%s" % ",".join(app.config['FBAPI_SCOPE'])
@@ -57,9 +60,9 @@ def fbapi_get_string(path, domain=u'graph', params=None, access_token=None,
 
 
 def fbapi_auth(code):
-    params = {'client_id': FBAPI_APP_ID,
+    params = {'client_id': APP_ID,
               'redirect_uri': get_home(),
-              'client_secret': FBAPI_APP_SECRET,
+              'client_secret': APP_SECRET,
               'code': code}
 
     result = fbapi_get_string(path=u"/oauth/access_token?", params=params,
@@ -76,8 +79,7 @@ def fbapi_get_application_access_token(id):
     token = fbapi_get_string(
         path=u"/oauth/access_token",
         params=dict(grant_type=u'client_credentials', client_id=id,
-                    client_secret=app.config['FB_APP_SECRET']),
-        domain=u'graph')
+                    client_secret=APP_SECRET, domain=u'graph'))
 
     token = token.split('=')[-1]
     if not str(id) in token:
@@ -98,9 +100,6 @@ def fql(fql, token, args=None):
 def fb_call(call, args=None):
     return json.loads(urllib2.urlopen("https://graph.facebook.com/" + call +
                                       '?' + urllib.urlencode(args)).read())
-
-app.config.from_object(__name__)
-app.config.from_object('conf.Config')
 
 
 def get_home():
@@ -127,7 +126,7 @@ def close():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    if app.config.get('FBAPI_APP_ID') and app.config.get('FBAPI_APP_SECRET'):
+    if APP_ID and APP_SECRET:
         app.run(host='0.0.0.0', port=port)
     else:
         print 'Cannot start application without Facebook App Id and Secret set'
