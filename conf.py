99901bf49a5b7e48948ca33ba6510191f6d031fb
Jon Plaut <jp@tangerinesymphony.net>
2012-01-17 13:00:49 -0800

    up and running

--- conf.py
+++ conf.py
@@ -7,5 +7,5 @@ class Config(object):
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
     FBAPI_SCOPE = ['user_likes', 'user_photos',
                    'user_photo_video_tags']
-    FBAPI_APP_ID = os.environ.get('FACEBOOK_APP_ID')
-    FBAPI_APP_SECRET = os.environ.get('FACEBOOK_SECRET')
+    FBAPI_APP_ID = "331389096895249"
+    FBAPI_APP_SECRET = "2f0ab20496fc3b232d0740694dd18d2e"
