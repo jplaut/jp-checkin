dd521e8518e5cce924ba9bf5091b23cf16b98d63
Jon Plaut <jp@tangerinesymphony.net>
2012-01-17 13:32:03 -0800

    adjusted variables to accomodate environment

--- mainApp.py
+++ mainApp.py
@@ -8,13 +8,19 @@ import urllib2
 from flask import Flask, request, redirect
 from mako.template import Template
 
-FBAPI_APP_ID = os.environ.get('FACEBOOK_APP_ID')
+app = Flask(__name__)
+if os.environ.get('FACEBOOK_APP_ID'):
+    FBAPI_APP_ID = os.environ.get('FACEBOOK_APP_ID')
+    FBAPI_APP_SECRET = os.environ.get('FACEBOOK_SECRET')
+else:
+    FBAPI_APP_ID = app.config['FBAPI_APP_ID']
+    FBAPI_APP_SECRET = FBAPI_APP_SECRET
 
 
 def oauth_login_url(preserve_path=True, next_url=None):
     fb_login_uri = ("https://www.facebook.com/dialog/oauth"
                     "?client_id=%s&redirect_uri=%s" %
-                    (app.config['FBAPI_APP_ID'], get_home()))
+                    (FBAPI_APP_ID, get_home()))
 
     if app.config['FBAPI_SCOPE']:
         fb_login_uri += "&scope=%s" % ",".join(app.config['FBAPI_SCOPE'])
@@ -51,9 +57,9 @@ def fbapi_get_string(path, domain=u'graph', params=None, access_token=None,
 
 
 def fbapi_auth(code):
-    params = {'client_id': app.config['FBAPI_APP_ID'],
+    params = {'client_id': FBAPI_APP_ID,
               'redirect_uri': get_home(),
-              'client_secret': app.config['FBAPI_APP_SECRET'],
+              'client_secret': FBAPI_APP_SECRET,
               'code': code}
 
     result = fbapi_get_string(path=u"/oauth/access_token?", params=params,
@@ -93,7 +99,6 @@ def fb_call(call, args=None):
     return json.loads(urllib2.urlopen("https://graph.facebook.com/" + call +
                                       '?' + urllib.urlencode(args)).read())
 
-app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_object('conf.Config')
 
