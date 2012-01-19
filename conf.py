90e3d275c560654e1a4086569aad22c50aa745f3
Jon Plaut <jp@tangerinesymphony.net>
2012-01-18 19:20:24 -0800

    app now displays tuple list of friends' checkins and aggregates duplicate locations

--- conf.py
+++ conf.py
@@ -5,7 +5,6 @@ class Config(object):
     DEBUG = True
     TESTING = False
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
-    FBAPI_SCOPE = ['user_likes', 'user_photos',
-                   'user_photo_video_tags']
+    FBAPI_SCOPE = ['user_checkins', 'friends_checkins']
     FBAPI_APP_ID = "331389096895249"
     FBAPI_APP_SECRET = "2f0ab20496fc3b232d0740694dd18d2e"
--- mainApp.py
+++ mainApp.py
@@ -4,6 +4,7 @@ import os.path
 import simplejson as json
 import urllib
 import urllib2
+from collections import defaultdict
 
 from flask import Flask, request, redirect
 from mako.template import Template
@@ -13,120 +14,136 @@ app.config.from_object(__name__)
 app.config.from_object('conf.Config')
 
 if os.environ.get('FACEBOOK_APP_ID'):
-    APP_ID = os.environ.get('FACEBOOK_APP_ID')
-    APP_SECRET = os.environ.get('FACEBOOK_SECRET')
+	APP_ID = os.environ.get('FACEBOOK_APP_ID')
+	APP_SECRET = os.environ.get('FACEBOOK_SECRET')
 else:
-    APP_ID = app.config.get('FBAPI_APP_ID')
-    APP_SECRET = app.config.get('FBAPI_APP_SECRET')
+	APP_ID = app.config.get('FBAPI_APP_ID')
+	APP_SECRET = app.config.get('FBAPI_APP_SECRET')
 
 
 def oauth_login_url(preserve_path=True, next_url=None):
-    fb_login_uri = ("https://www.facebook.com/dialog/oauth"
-                    "?client_id=%s&redirect_uri=%s" %
-                    (APP_ID, get_home()))
+	fb_login_uri = ("https://www.facebook.com/dialog/oauth"
+					"?client_id=%s&redirect_uri=%s" %
+					(APP_ID, get_home()))
 
-    if app.config['FBAPI_SCOPE']:
-        fb_login_uri += "&scope=%s" % ",".join(app.config['FBAPI_SCOPE'])
-    return fb_login_uri
+	if app.config['FBAPI_SCOPE']:
+		fb_login_uri += "&scope=%s" % ",".join(app.config['FBAPI_SCOPE'])
+	return fb_login_uri
 
 
 def simple_dict_serialisation(params):
-    return "&".join(map(lambda k: "%s=%s" % (k, params[k]), params.keys()))
+	return "&".join(map(lambda k: "%s=%s" % (k, params[k]), params.keys()))
 
 
 def base64_url_encode(data):
-    return base64.urlsafe_b64encode(data).rstrip('=')
+	return base64.urlsafe_b64encode(data).rstrip('=')
 
 
 def fbapi_get_string(path, domain=u'graph', params=None, access_token=None,
-                     encode_func=urllib.urlencode):
-    """Make an API call"""
-    if not params:
-        params = {}
-    params[u'method'] = u'GET'
-    if access_token:
-        params[u'access_token'] = access_token
+					 encode_func=urllib.urlencode):
+	"""Make an API call"""
+	if not params:
+		params = {}
+	params[u'method'] = u'GET'
+	if access_token:
+		params[u'access_token'] = access_token
 
-    for k, v in params.iteritems():
-        if hasattr(v, 'encode'):
-            params[k] = v.encode('utf-8')
+	for k, v in params.iteritems():
+		if hasattr(v, 'encode'):
+			params[k] = v.encode('utf-8')
 
-    url = u'https://' + domain + u'.facebook.com' + path
-    params_encoded = encode_func(params)
-    url = url + params_encoded
-    result = urllib2.urlopen(url).read()
+	url = u'https://' + domain + u'.facebook.com' + path
+	params_encoded = encode_func(params)
+	url = url + params_encoded
+	result = urllib2.urlopen(url).read()
 
-    return result
+	return result
 
 
 def fbapi_auth(code):
-    params = {'client_id': APP_ID,
-              'redirect_uri': get_home(),
-              'client_secret': APP_SECRET,
-              'code': code}
+	params = {'client_id': APP_ID,
+			  'redirect_uri': get_home(),
+			  'client_secret': APP_SECRET,
+			  'code': code}
 
-    result = fbapi_get_string(path=u"/oauth/access_token?", params=params,
-                              encode_func=simple_dict_serialisation)
-    pairs = result.split("&", 1)
-    result_dict = {}
-    for pair in pairs:
-        (key, value) = pair.split("=")
-        result_dict[key] = value
-    return (result_dict["access_token"], result_dict["expires"])
+	result = fbapi_get_string(path=u"/oauth/access_token?", params=params,
+							  encode_func=simple_dict_serialisation)
+	pairs = result.split("&", 1)
+	result_dict = {}
+	for pair in pairs:
+		(key, value) = pair.split("=")
+		result_dict[key] = value
+	return (result_dict["access_token"], result_dict["expires"])
 
 
 def fbapi_get_application_access_token(id):
-    token = fbapi_get_string(
-        path=u"/oauth/access_token",
-        params=dict(grant_type=u'client_credentials', client_id=id,
-                    client_secret=APP_SECRET, domain=u'graph'))
+	token = fbapi_get_string(
+		path=u"/oauth/access_token",
+		params=dict(grant_type=u'client_credentials', client_id=id,
+					client_secret=APP_SECRET, domain=u'graph'))
 
-    token = token.split('=')[-1]
-    if not str(id) in token:
-        print 'Token mismatch: %s not in %s' % (id, token)
-    return token
+	token = token.split('=')[-1]
+	if not str(id) in token:
+		print 'Token mismatch: %s not in %s' % (id, token)
+	return token
 
 
 def fql(fql, token, args=None):
-    if not args:
-        args = {}
+	if not args:
+		args = {}
 
-    args["query"], args["format"], args["access_token"] = fql, "json", token
-    return json.loads(
-        urllib2.urlopen("https://api.facebook.com/method/fql.query?" +
-                        urllib.urlencode(args)).read())
-
+	args["query"], args["format"], args["access_token"] = fql, "json", token
+	return json.loads(
+		urllib2.urlopen("https://api.facebook.com/method/fql.query?" +
+						urllib.urlencode(args)).read())
 
 def fb_call(call, args=None):
-    return json.loads(urllib2.urlopen("https://graph.facebook.com/" + call +
-                                      '?' + urllib.urlencode(args)).read())
+	return json.loads(urllib2.urlopen("https://graph.facebook.com/" + call +
+									  '?' + urllib.urlencode(args)).read())
 
 
 def get_home():
-    return 'http://' + request.host + '/'
+	return 'http://' + request.host + '/'
+
+def aggregate_checkin_location(checkins_unsorted):
+	checkins_tuple=[]
+	for checkin in checkins_unsorted:
+		checkins_tuple.append((checkin['page_id'], checkin['author_uid']))
+		
+	checkins_sorted={}
+	d = defaultdict(list)
+	
+	for k, v in checkins_tuple:
+		if not v in d[k]:
+			d[k].append(int(str(v).rstrip('L')))
+		
+
+	return dict(d)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    print get_home()
-    if request.args.get('code', None):
-        access_token = fbapi_auth(request.args.get('code'))[0]
+	print get_home()
+	if request.args.get('code', None):
+		access_token = fbapi_auth(request.args.get('code'))[0]
 
-        checkins = fb_call('me/friends', args={'access_token': access_token})
+		checkins = fql("SELECT author_uid, page_id FROM checkin WHERE author_uid IN (SELECT uid2 FROM friend WHERE uid1=me())", access_token)
+		checkins_sorted = aggregate_checkin_location(checkins)
+	
 
-        return Template(filename='templates/index.html').render(checkins=checkins)
+		return Template(filename='templates/index.html').render(checkins_sorted=checkins_sorted)
 
-    else:
-        print oauth_login_url(next_url=get_home())
-        return redirect(oauth_login_url(next_url=get_home()))
+	else:
+		print oauth_login_url(next_url=get_home())
+		return redirect(oauth_login_url(next_url=get_home()))
 
 @app.route('/close/', methods=['GET', 'POST'])
 def close():
-    return render_template('close.html')
+	return render_template('close.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    if APP_ID and APP_SECRET:
-        app.run(host='0.0.0.0', port=port)
-    else:
-        print 'Cannot start application without Facebook App Id and Secret set'
+	port = int(os.environ.get("PORT", 5000))
+	if APP_ID and APP_SECRET:
+		app.run(host='0.0.0.0', port=port)
+	else:
+		print 'Cannot start application without Facebook App Id and Secret set'
